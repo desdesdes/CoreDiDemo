4d5fcc44006ee06aa07197d6f806b76192728cf4
Bart Vries <bart.vries@afas.nl>
2024-12-17 17:51:54 +0100

    ﻿Final

---
+++ Afas.Bvr.Crm/PhonenumberChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+namespace Afas.Bvr.Crm;
+
+public abstract class PhonenumberChecker
+{
+  public abstract Task<bool> CheckPhoneNumber(string? phoneNumber);
+}
+
+public class WebPhonenumberChecker : PhonenumberChecker
+{
+  private readonly HttpClient _httpClient;
+
+  public WebPhonenumberChecker(HttpClient httpClient)
+  {
+    _httpClient = httpClient;
+  }
+
+  public override async Task<bool> CheckPhoneNumber(string? phoneNumber)
+  {
+    return !string.IsNullOrEmpty(phoneNumber) &&
+      await _httpClient.GetFromJsonAsync<bool>($"https://coredidemo.azurewebsites.net/phonenumbercheck.html?number={Uri.EscapeDataString(phoneNumber)}");
+  }
+}
